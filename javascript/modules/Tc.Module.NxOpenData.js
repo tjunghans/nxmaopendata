d012de0bc0ef26a732e8d9aabf341a833e76567e
Thomas Junghans <thomas.junghans@gmail.com>
2013-11-07 16:58:29 +0100

    Update clustering

--- javascript/modules/Tc.Module.NxOpenData.js
+++ javascript/modules/Tc.Module.NxOpenData.js
@@ -12,6 +12,19 @@
 	Tc.Module.NxOpenData = Tc.Module.extend({
 
 		/**
+		 * Distance in km
+		 *
+		 * @type number
+		 */
+		clusterDistance : 10,
+
+		/**
+		 *
+		 * @type Cluster
+		 */
+		clusters : [],
+
+		/**
 		 * Hook function to do all of your module stuff.
 		 *
 		 * @method on
@@ -50,14 +63,41 @@
 
 			$ctx.on('dataavailable', function (e, data) {
 
-
+				console.log('Entries : ', data.length);
 				_.each(data, function (item, key, list) {
-					console.log('lat', item.properties.geo_latitude);
-					console.log('lon', item.properties.geo_longitude);
+
+					var itemLat = item.properties.geo_latitude,
+						itemLon = item.properties.geo_longitude,
+						itemPoint = new Point(itemLat, itemLon);
+
+					// First check clusters
+					var cluster = mod.findClusterWithinDistance(itemPoint, mod.clusterDistance);
+
+					// If no cluster is found, a new one is created
+					if (cluster === null) {
+
+						//mod.getLatLonWithinDistance(itemLat, itemLon, data, mod.clusterDistance);
+
+						var newCluster = new Cluster(itemPoint);
+
+						mod.clusters.push(newCluster);
+
+					} else {
+						cluster.addPoint(itemPoint);
+					}
+
 				});
 
+				console.log('Clusters :', mod.clusters.length);
+				_.each(mod.clusters, function (cluster) {
+					console.log(cluster.getCenter(), cluster.getPoints().length);
+				});
 
 			});
+
+			/**
+			 * For each latlng iterate through existing clusters (get their center)
+			 */
 
 			$.ajax({
 				url : url,
@@ -68,14 +108,6 @@
 				}
 			});
 
-			// Testing
-			console.log(mod.calculateDistance(47.345441, 8.606525, 47.6464245, 9.1725561));
-
-			// Iterate over all lat lng
-
-
-
-
 			callback();
 		},
 
@@ -83,7 +115,109 @@
 			var p1 = new LatLon(lat1, lon1);
 			var p2 = new LatLon(lat2, lon2);
 			return p1.distanceTo(p2);          // in km
+		},
+
+		/**
+		 * @param {number} lat
+		 * @param {number} lon
+		 * @param {object} data as received (json)
+		 * @param {number} distance in km
+		 */
+		getLatLonWithinDistance : function (lat, lon, data, distance) {
+			var mod = this,
+				latLonWithinDistance = [];
+
+			_.each(data, function (item, key, list) {
+
+				var itemLat = item.properties.geo_latitude,
+					itemLon = item.properties.geo_longitude;
+
+				//mod.calculateDistance(lat, lon, itemLat, itemLon), distance);
+			});
+
+		},
+
+		/**
+		 *
+		 * @param {Point} point
+		 * @param {number} distance
+		 * @returns {null|Cluster} returns cluster on success
+		 */
+		findClusterWithinDistance : function (point, distance) {
+			var mod = this;
+
+			for (var i = 0, ii = mod.clusters.length; i < ii; i++) {
+				var center = mod.clusters[i].getCenter();
+
+				if (mod.calculateDistance(point.lat, point.lon, center.lat, center.lon) <= distance) {
+					return mod.clusters[i];
+				}
+			}
+
+			return null;
 		}
 
 	});
+
+	function Point(lat, lon) {
+		this.init(lat, lon);
+	}
+
+	Point.prototype = {
+		constructor : Point,
+		init : function (lat, lon) {
+			this.lat = lat;
+			this.lon = lon;
+		}
+	};
+
+	function Cluster(point) {
+		this.points = [];
+		this.center = null;
+		this.init(point);
+	}
+
+	Cluster.prototype = {
+		constructor : Cluster,
+		init : function (point) {
+
+			this.addPoint(point);
+			this.center = this.getCenter();
+		},
+
+		/**
+		 *
+		 * @param {Point} point
+		 */
+		addPoint : function (point) {
+			this.points.push(point);
+			this._setCenter();
+		},
+
+		_determineCenter : function (points) {
+			var pointsCount = points.length;
+			var latTotal = 0;
+			var lonTotal = 0;
+
+			_.each(points, function (point, index, list) {
+				latTotal += point.lat;
+				lonTotal += point.lon;
+			});
+
+			return new Point(latTotal / pointsCount, lonTotal / pointsCount);
+
+		},
+
+		_setCenter : function () {
+			this.center = this._determineCenter(this.points);
+		},
+
+		getCenter : function () {
+			return this.center;
+		},
+
+		getPoints : function () {
+			return this.points;
+		}
+	}
 }(Tc.$, window));
--- test/javascript/exampleTest.js
+++ test/javascript/exampleTest.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 /*
-	module("Minesweeper", {
+	module("Example", {
 		setup: function() {
 		}, teardown: function() {
 		}
